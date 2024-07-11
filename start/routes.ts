f208433907fdcc0595ef078270e5c38814048466
20Frederic20 <fredericpierrothountineme@gmail.com>
2024-07-11 19:23:29 +0100

    Revert "implement categories"
    This reverts commit f8dcbe05e31f632ae664d187552c39aab33e2f45.

--- start/routes.ts
+++ start/routes.ts
@@ -11,7 +11,6 @@ import User from '#models/user'
 import { middleware } from '#start/kernel'
 import router from '@adonisjs/core/services/router'
 import { HttpContext } from '@adonisjs/core/http'
-import Category from '#models/category'
 router.on('/').render('pages/home')
 
 router
@@ -23,75 +22,29 @@ router
     return view.render('pages/index')
   })
   .use(middleware.auth())
-  .as('index')
 
-router
-  .get('/about', async (ctx) => {
-    return ctx.view.render('pages/about')
-  })
-  .as('about')
+router.get('/about', async (ctx) => {
+  return ctx.view.render('pages/about')
+})
 
-router
-  .get('/contact', async (ctx) => {
-    return ctx.view.render('pages/contact')
-  })
-  .as('contact')
+router.get('/contact', async (ctx) => {
+  return ctx.view.render('pages/contact')
+})
 
-router
-  .get('/users/create', async (ctx) => {
-    return ctx.view.render('pages/users/create')
-  })
-  .as('users.create')
+router.get('/users/create', async (ctx) => {
+  return ctx.view.render('pages/users/create')
+})
 
-router
-  .post('/users', async ({ request, auth, response }: HttpContext) => {
-    const data = request.only(['last_name', 'first_name', 'email', 'password'])
-    const user = await User.create(data)
-    await auth.use('web').login(user)
-    return response.redirect('/index')
-  })
-  .as('users.store')
+router.post('/users', async ({ request, auth, response }: HttpContext) => {
+  const data = request.only(['last_name', 'first_name', 'email', 'password'])
+  const user = await User.create(data)
+  await auth.use('web').login(user)
+  return response.redirect('/index')
+})
 
-router
-  .get('/login', async ({ view }: HttpContext) => {
-    return view.render('pages/users/login')
-  })
-  .as('users.login.form')
-
-router
-  .post('/login', async ({ request, auth, response }: HttpContext) => {
-    const { email, password } = request.only(['email', 'password'])
-    const user = await User.verifyCredentials(email, password)
-    await auth.use('web').login(user)
-    return response.redirect('/index')
-  })
-  .as('users.login')
-
-router
-  .post('logout', async ({ auth, response }) => {
-    await auth.use('web').logout()
-    return response.redirect('/login')
-  })
-  .use(middleware.auth())
-  .as('users.logout')
-
-router
-  .get('/categories', async ({ view }: HttpContext) => {
-    const categories = await Category.all()
-    return view.render('pages/categories/index', { categories: categories })
-  })
-  .as('categories.index')
-
-router
-  .get('/categories/create', async (ctx) => {
-    return ctx.view.render('pages/categories/create')
-  })
-  .as('categories.create')
-
-router
-  .post('/categories', async ({ request, response }: HttpContext) => {
-    let data = request.only(['slug', 'name'])
-    await Category.create(data)
-    return response.redirect('/index')
-  })
-  .as('categories.store')
+router.get('/login', async ({ request, auth, view }: HttpContext) => {
+  const { email, password } = request.only(['email', 'password'])
+  const user = await User.verifyCredentials(email, password)
+  await auth.use('web').login(user)
+  return view.render('pages/users/login')
+})
